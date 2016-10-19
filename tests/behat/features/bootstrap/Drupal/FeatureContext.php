755a3e174e9e3e6dc1eec5ec594722fced8b6c66
Adam Weingarten <aweingarten@users.noreply.github.com>
2016-10-19 12:35:31 -0400

    Initial commit.

---
+++ tests/behat/features/bootstrap/Drupal/FeatureContext.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Drupal;
+
+use Drupal\DrupalExtension\Context\RawDrupalContext;
+use Behat\Behat\Context\SnippetAcceptingContext;
+
+/**
+ * FeatureContext class defines custom step definitions for Behat.
+ */
+class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext {
+
+  /**
+   * Every scenario gets its own context instance.
+   *
+   * You can also pass arbitrary arguments to the
+   * context constructor through behat.yml.
+   */
+  public function __construct() {
+
+  }
+
+}
