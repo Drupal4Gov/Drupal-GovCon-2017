bf139a7abc871bfba215869b31fd45f016889460
Lindsey Kopacz <lkopacz@users.noreply.github.com>
2017-01-30 19:32:52 -0500

    DGC-138 Added Hero Block, Updated Block Template using Components, Generated DGC Hero block config.

--- docroot/modules/custom/capitalcamp_blocks/src/Plugin/Block/DgcHero.php
+++ docroot/modules/custom/capitalcamp_blocks/src/Plugin/Block/DgcHero.php
@@ -20,7 +20,7 @@ class DgcHero extends BlockBase {
   public function build() {
     return array(
       '#type' => 'markup',
-      '#markup' => $this->t("This block's title will be changed to uppercase. Any other block with 'uppercase' in the subject or title will also be altered. If you change this block's title through the UI to omit the word 'uppercase', it will still be altered to uppercase as the subject key has not been changed."),
+      '#markup' => '',
     );
   }
 
