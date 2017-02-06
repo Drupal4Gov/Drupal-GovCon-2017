888a8cce4e2d1325f80fd00cf2928942c467a8ab
Jon Raedeke <jonraedeke@gmail.com>
2017-02-06 00:47:45 -0500

    DGC-141 - Fix indent size to match coding standards

--- docroot/modules/custom/capitalcamp_blocks/src/Plugin/Block/DgcNav.php
+++ docroot/modules/custom/capitalcamp_blocks/src/Plugin/Block/DgcNav.php
@@ -16,46 +16,50 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  *  admin_label = @Translation("DGC Nav"),
  * )
  */
-class DgcNav extends BlockBase implements ContainerFactoryPluginInterface {
+class DgcNav extends BlockBase implements ContainerFactoryPluginInterface
+{
 
-    /**
-     * {@inheritdoc}
-     */
-    public function __construct(array $configuration, $plugin_id, $plugin_definition, MenuLinkTreeInterface $menu_tree) {
-        parent::__construct($configuration, $plugin_id, $plugin_definition);
-        $this->menuTree = $menu_tree;
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, MenuLinkTreeInterface $menu_tree)
+  {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->menuTree = $menu_tree;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition)
+  {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('menu.link_tree')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build()
+  {
+    $parameters = new MenuTreeParameters();
+    $tree = $this->menuTree->load('main', $parameters);
+    $processedFooter = [];
+
+    // Normalize the menu so that we don't need to in twig.
+    foreach ($tree as $treeElement) {
+      $currentParsedRecord = [];
+      $currentParsedRecord['text'] = $treeElement->link->getTitle();
+      $currentParsedRecord['url'] = $treeElement->link->getUrlObject();
+      $processedFooter[] = $currentParsedRecord;
     }
 
-    /**
-     * {@inheritdoc}
-     */
-    public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-        return new static(
-            $configuration,
-            $plugin_id,
-            $plugin_definition,
-            $container->get('menu.link_tree')
-        );
-    }
-
-    /**
-     * {@inheritdoc}
-     */
-    public function build() {
-        $parameters = new MenuTreeParameters();
-        $tree = $this->menuTree->load('main', $parameters);
-        $processedFooter = [];
-
-        // Normalize the menu so that we don't need to in twig.
-        foreach ($tree as $treeElement) {
-            $currentParsedRecord = [];
-            $currentParsedRecord['text'] = $treeElement->link->getTitle();
-            $currentParsedRecord['url'] = $treeElement->link->getUrlObject();
-            $processedFooter[] = $currentParsedRecord;
-        }
-
-        $build['nav_links'] = $processedFooter;
-        return $build;
-    }
+    $build['nav_links'] = $processedFooter;
+    return $build;
+  }
 
 }
