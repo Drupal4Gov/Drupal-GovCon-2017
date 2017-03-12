398e2d3ffdf5129377060bf8ec4b41e78e4ceb3d
Adam Weingarten <adam.weingarten@acquia.com>
2017-03-12 14:22:38 -0400

    DGC-232: Added secondary hero block to secondary pages.

---
+++ docroot/modules/custom/capitalcamp_blocks/src/Plugin/Block/DgcSecondaryHero.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\capitalcamp_blocks\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides Secondary Hero Block.
+ *
+ * @Block(
+ *   id = "dgc_secondary_hero",
+ *   admin_label = @Translation("DGC Secondary Page Hero")
+ * )
+ */
+class DgcSecondaryHero extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    return array(
+      '#type' => 'markup',
+      '#markup' => '',
+    );
+  }
+
+}
