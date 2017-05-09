eb55964a0ee8812c0393e2fe9af10708911668a8
Lindsey Kopacz <lkopacz@users.noreply.github.com>
2017-05-08 21:48:44 -0400

    DGC-350: Fixed sniffing errors in JavaScript.

--- docroot/themes/custom/twentyseventeen/js/src/theme.js
+++ docroot/themes/custom/twentyseventeen/js/src/theme.js
@@ -12,12 +12,12 @@
       if (typeof context['location'] !== 'undefined') { // Only fire on document load.
         var secondLevelNav = $('.nav__items--second-level').children();
         var secondLevelNavLinks = secondLevelNav.children();
-        secondLevelNavLinks.focus( function() {
-          //changes the CSS of the ul to appear on focus.
+        secondLevelNavLinks.focus(function () {
+          // Changes the CSS of the ul to appear on focus.
           $(this).parent().parent().addClass('focus');
         });
-        secondLevelNavLinks.blur( function() {
-          //controls the CSS of the ul when you exit out of that particular submenu
+        secondLevelNavLinks.blur(function () {
+          // Controls the CSS of the ul when you exit out of that particular submenu.
           $(this).parent().parent().removeClass('focus');
         });
       }
