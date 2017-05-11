31537e879da2d41c7c7eedb9360897484d922f88
Brooke Heaton <bheaton@forumone.com>
2017-05-11 15:59:37 -0400

    dgc-310: Make ‘Country’ required and change registration-form.js to respond to value of field.

--- docroot/modules/custom/capitalcamp_glue/libraries/js/registration-form.js
+++ docroot/modules/custom/capitalcamp_glue/libraries/js/registration-form.js
@@ -3,7 +3,7 @@
     Drupal.behaviors.registration_form = {
         attach: function (context) {
             // Conditionally hide the city/state unless user chooses United States.
-            $("#edit-address-country").change(function () {
+            $("#edit-address-country").value(function () {
                 if ($("#edit-address-country").val() === "United States") {
                     $('.form-item-address-city').show();
                     $('.form-item-address-state-province').show();
