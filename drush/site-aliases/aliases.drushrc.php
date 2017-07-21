714b6d25332def04e023018d7eddd1e6feab12e0
Mike Madison <mike.madison@acquia.com>
2017-07-21 06:09:39 -0700

    DGC-000: updating to BLT 8.9.x.

--- drush/site-aliases/aliases.drushrc.php
+++ drush/site-aliases/aliases.drushrc.php
@@ -358,14 +358,19 @@
 #$aliases['live'] = array(
 #    'uri' => 'http://example.com',
 #    'root' => $aliases['dev']['root'],
-#  ) + $aliases['server'];// [vagrant_machine_name].local
+#  ) + $aliases['server'];
+
+// Local environment.
 $aliases['capitalcamp.local'] = array(
-  // /var/www/[vagrant_machine_name]/docroot
   'root' => '/var/www/capitalcamp/docroot',
-  // vagrant_hostname
   'uri' => 'http://local.capitalcamp.com',
-  // vagrant_hostname
-  'remote-host' => 'local.capitalcamp.com',
-  'remote-user' => 'vagrant',
-  'ssh-options' => '-o PasswordAuthentication=no -i ' . drush_server_home() . '/.vagrant.d/insecure_private_key'
-);
+  );
+// Add remote connection options when alias is used outside VM.
+if ('vagrant' != $_SERVER['USER']) {
+  $aliases['capitalcamp.local'] += array(
+    'remote-host' => 'local.capitalcamp.com',
+    'remote-user' => 'vagrant',
+    'ssh-options' => '-o PasswordAuthentication=no -i ' . drush_server_home() . '/.vagrant.d/insecure_private_key'
+  );
+}
+
