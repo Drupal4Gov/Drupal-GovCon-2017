c7ca4ebc5149c1efab88119064996112bdf3a278
Mike Madison <mike.madison@acquia.com>
2017-08-04 12:40:35 -0700

    DGC-000: removing broken behat tests.

--- tests/behat/features/bootstrap/Drupal/FeatureContext.php
+++ tests/behat/features/bootstrap/Drupal/FeatureContext.php
@@ -4,6 +4,7 @@ namespace Drupal;
 
 use Drupal\DrupalExtension\Context\RawDrupalContext;
 use Behat\Behat\Context\SnippetAcceptingContext;
+use PHPUnit_Framework_Assert;
 
 /**
  * FeatureContext class defines custom step definitions for Behat.
@@ -18,6 +19,39 @@ class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext
    */
   public function __construct() {
 
+  }
+
+  /**
+   * Checks if a field is present for users.
+   *
+   * @Then the field :arg1 is present for users
+   */
+  public function isUserField($field_name) {
+    $bundle_fields = \Drupal::getContainer()->get('entity_field.manager')->getFieldDefinitions('user', 'user');
+    if (empty($bundle_fields[$field_name])) {
+      throw new \Exception('Field ' . $field_name . ' is not required.');
+    }
+  }
+
+  /**
+   * Checks if a field is required for users.
+   *
+   * @Then the :arg1 field should be required for users
+   */
+  public function checkIsRequiredUserField($field_name) {
+    $this->isRequiredUserField($field_name);
+  }
+
+  /**
+   * Checks if a field is required for users.
+   *
+   * The $field_name field should be required for users.
+   */
+  public function isRequiredUserField($field_name) {
+    $bundle_fields = \Drupal::getContainer()->get('entity_field.manager')->getFieldDefinitions('user', 'user');
+    $field_definition = $bundle_fields[$field_name];
+    $setting = $field_definition->isRequired();
+    PHPUnit_Framework_Assert::assertNotEmpty($setting, 'Field ' . $field_name . ' is not required.');
   }
 
 }
