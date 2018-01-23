9ae04be001722a1091a6f926e0b182b07c9b21f5
Lindsey Kopacz <lindsey.kopacz@acquia.com>
2018-01-23 16:29:13 -0500

    DGC-180: Fixed build slowness and updated packages to assure better gulp tasks.

--- docroot/themes/custom/twentyeighteen/gulp-tasks/build.js
+++ docroot/themes/custom/twentyeighteen/gulp-tasks/build.js
@@ -7,24 +7,29 @@
 
 module.exports = function (gulp, plugins, options) {
   'use strict';
+  plugins.runSequence.options.showErrorStackTrace = false;
 
-  gulp.task('build', [
-    'compile:sass',
-    'webpack:js',
-    'minify:css',
-    'compile:styleguide',
-    'lint:js-gulp',
-    'lint:js-with-fail',
-    'lint:css-with-fail'
-  ]);
+  gulp.task('build', function(cb) {
+    plugins.runSequence(
+        'compile:sass',
+        'webpack:js',
+        ['minify:css',
+          'compile:styleguide'],
+        ['lint:js-gulp',
+          'lint:js-with-fail',
+          'lint:css-with-fail'],
+        cb);
+  });
 
-  gulp.task('build:dev', [
-    'compile:sass',
-    'webpack:js',
-    'minify:css',
-    'compile:styleguide',
-    'lint:js-gulp',
-    'lint:js',
-    'lint:css'
-  ]);
+  gulp.task('build:dev', function(cb) {
+    plugins.runSequence(
+        'compile:sass',
+        'webpack:js',
+        ['minify:css',
+          'compile:styleguide'],
+        ['lint:js-gulp',
+          'lint:js',
+          'lint:css'],
+        cb);
+  });
 };
