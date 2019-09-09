edb9465a065265cb3ad803875521469810389ddb
Mike Madison <mike.madison@acquia.com>
2019-09-09 19:49:13 -0400

    DGC-000: testing backstop.

---
+++ tests/backstop/html_report/config.js
@@ -0,0 +1,78 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../reference/govcon_BackstopJS_Homepage_0_document_0_phone.png",
+        "test": "../test/20190909-200756/govcon_BackstopJS_Homepage_0_document_0_phone.png",
+        "selector": "document",
+        "fileName": "govcon_BackstopJS_Homepage_0_document_0_phone.png",
+        "label": "BackstopJS Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://garris.github.io/BackstopJS/",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "phone",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../reference/govcon_BackstopJS_Homepage_0_document_1_tablet.png",
+        "test": "../test/20190909-200756/govcon_BackstopJS_Homepage_0_document_1_tablet.png",
+        "selector": "document",
+        "fileName": "govcon_BackstopJS_Homepage_0_document_1_tablet.png",
+        "label": "BackstopJS Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://garris.github.io/BackstopJS/",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "tablet",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "../reference/govcon_BackstopJS_Homepage_0_document_2_desktop.png",
+        "test": "../test/20190909-200756/govcon_BackstopJS_Homepage_0_document_2_desktop.png",
+        "selector": "document",
+        "fileName": "govcon_BackstopJS_Homepage_0_document_2_desktop.png",
+        "label": "BackstopJS Homepage",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "https://garris.github.io/BackstopJS/",
+        "referenceUrl": "",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "misMatchPercentage": "0.00"
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "govcon"
+});
