f063ff81c31419891b30aacc8e2782ed99d0c842
Candice Dexter <candice.n.dexter@gmail.com>
2020-08-24 17:18:17 -0400

    DGC-000: Disable script for older themes.

--- blt/scripts/frontend-build.sh
+++ blt/scripts/frontend-build.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-echo "running frontend build for twentyseventeen"
-(cd docroot/themes/custom/twentyseventeen;npm run build)
+# echo "running frontend build for twentyseventeen"
+# (cd docroot/themes/custom/twentyseventeen;npm run build)
 
-echo "running frontend build for twentyeighteen"
-(cd docroot/themes/custom/twentyeighteen;npm run build)
+# echo "running frontend build for twentyeighteen"
+# (cd docroot/themes/custom/twentyeighteen;npm run build)
 
 echo "running frontend build for twentynineteen"
 (cd docroot/themes/custom/twentynineteen;npm run build)
--- blt/scripts/frontend-setup.sh
+++ blt/scripts/frontend-setup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-echo "running frontend setup for twentyseventeen"
-(cd docroot/themes/custom/twentyseventeen;npm run install-tools)
+# echo "running frontend setup for twentyseventeen"
+# (cd docroot/themes/custom/twentyseventeen;npm run install-tools)
 
-echo "running frontend setup for twentyeighteen"
-(cd docroot/themes/custom/twentyeighteen;npm run install-tools)
+# echo "running frontend setup for twentyeighteen"
+# (cd docroot/themes/custom/twentyeighteen;npm run install-tools)
 
 echo "running frontend setup for twentynineteen"
 (cd docroot/themes/custom/twentynineteen;npm run install-tools)
