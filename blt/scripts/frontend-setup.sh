6c4e4e703603b734535f9d0c7112bbed465903a0
Mike Madison <mike.madison@acquia.com>
2020-09-23 18:42:10 -0700

    DGC-000: temporary blt hack.

--- blt/scripts/frontend-setup.sh
+++ blt/scripts/frontend-setup.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 
-echo "running frontend setup for twentyseventeen"
-(cd docroot/themes/custom/twentyseventeen;npm run install-tools)
-
-echo "running frontend setup for twentyeighteen"
-(cd docroot/themes/custom/twentyeighteen;npm run install-tools)
-
 echo "running frontend setup for twentynineteen"
 (cd docroot/themes/custom/twentynineteen;npm run install-tools)
